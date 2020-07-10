5105e5e70352267f2503165c19db06fcade6e4aa
luis1184lopez <62674480+luis1184lopez@users.noreply.github.com>
2020-07-10 03:40:43 +0200

    Update micomponente.component.ts

--- micomponente.component.ts
+++ micomponente.component.ts
@@ -3,10 +3,14 @@ import {Component} from '@angular/core';
 @Component ({
 selector: 'app-micomponente',
 templateUrl: './micomponente.component.html',
-styles: ['h3 { font-weight: normal; color:darkgray }']
+styles: ['h1 { font-weight: normal; color:darkgray }']
 
 })
 export class micomponente{
-    hoy:any = new Date();
+  Nombre: string='Luis';
+  Apellido: string='Lopez';
+  Departamento: string='Masaya';
+  Ciudad: string='Masatepe'; 
     
 }
+
